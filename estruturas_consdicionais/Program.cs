f4ee9b8d5e6865f731d2582403a8bbf9c29cc7d7
[lince_academye] <[julianokeil@gmail.com]>
2023-10-01 17:56:22 -0300

    csharp

---
+++ estruturas_consdicionais/Program.cs
@@ -0,0 +1,56 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+
+
+        //List<int> list = new List<int> { 1,2,3,4,5 };
+
+        // for (int i = 0; i < list.Count; i++)
+        // {
+        //     Console.WriteLine(list[i]);
+        // }
+
+        //string[] nomes = { "leo", "juliano", "emyly", "caua", "rodrigo" };
+
+        //foreach (var item in nomes)
+        //{
+        //    Console.WriteLine(item);
+        //}
+
+        Console.WriteLine("digite um numero");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        if (numero < 5)
+        {
+            Console.WriteLine("numero menor que 5");
+        }
+        else if (numero < 10)
+        {
+            Console.WriteLine("numero menor que 10");
+        }
+        else if (numero < 20)
+        {
+            Console.WriteLine("numero menor que 20");
+        }
+        else
+        {
+            Console.WriteLine("numero maior que 20");
+        }
+
+        //switch (numero)
+        //{
+        //    case < 5 : Console.WriteLine("numero menor que 5");
+        //        break;
+        //    case < 10:
+        //        Console.WriteLine("numero menor que 10");
+        //        break;
+        //    case < 20:
+        //        Console.WriteLine("numero menor que 20");
+        //        break;
+
+        //}
+
+
+    }
+}
